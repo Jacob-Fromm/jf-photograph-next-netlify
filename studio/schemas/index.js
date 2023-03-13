181b88c5d6f5166923383e8cbd317e1a10a916bc
Jacob Fromm <jacobfromm@gmail.com>
2023-03-13 13:47:57 -0400

    cleaned up schemas

---
+++ studio/schemas/index.js
@@ -0,0 +1,22 @@
+// Document types
+import category from './documents/category'
+import sampleProject from './documents/sampleProject'
+import siteSettings from './documents/siteSettings'
+
+// Object types
+import bioPortableText from './objects/bioPortableText'
+import figure from './objects/figure'
+import projectPortableText from './objects/projectPortableText'
+import simplePortableText from './objects/simplePortableText'
+import gallery from './objects/gallery'
+
+export const schemaTypes = [
+  category,
+  sampleProject,
+  siteSettings,
+  bioPortableText,
+  figure,
+  projectPortableText,
+  simplePortableText,
+  gallery,
+]
