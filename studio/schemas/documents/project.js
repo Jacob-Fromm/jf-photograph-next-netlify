88efec61e0a1e2280e8c55c28fd52cca17916f26
Jacob Fromm <jacobfromm@gmail.com>
2023-03-15 15:37:36 -0400

    updated schema

--- studio/schemas/documents/project.js
+++ studio/schemas/documents/project.js
@@ -29,6 +29,16 @@ export default {
       name: 'mainImage',
       title: 'Main image',
       type: 'image',
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: 'alt',
+          type: 'string',
+          title: 'Alternative text',
+        },
+      ],
     },
     {
       name: 'photos',
